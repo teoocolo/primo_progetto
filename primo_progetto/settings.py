40ae514975a2d03f4732e74c8ec6d18f3e35ef59
teoocolo <teuzzzz257@gmail.com>
2025-10-20 13:46:30 +0200

    aggiornato parametro allowed host

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5*^s104p25oh&78)&x#m14&f3j#9h(j1k2@^5$b-mvam#wxrv$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
